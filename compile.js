4cadc9ff6790dfcc95428ca0328ee47b3e243978
Tony Thompson <penguinprofession@icloud.com>
2018-05-30 16:05:19 -0400

    configured compile script to export ABI and bytecode

---
+++ compile.js
@@ -0,0 +1,16 @@
+const path = require('path');
+//helps build path from current directory to .sol file, we are guranteed crossed platform compatibility
+
+const solc = require('solc');
+const fs = require('fs');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
+//this will take you from the home directory on machine to inbox and contracts folder.
+
+const source = fs.readFileSync(inboxPath, 'utf8');
+//reads raw source code from our contract by creating source using fs module. 
+
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
+//exports objects details (contracts property inbox)
+//return value will always be an object, top level contracts{ inbox...}
+//provides bytecode and interface (ABI) - list out all functions that can be called, arguements, return values, etc. 
