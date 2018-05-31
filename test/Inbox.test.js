f566e03e83b4028eebd72a47b15194c00d948b74
Tony Thompson <penguinprofession@icloud.com>
2018-05-31 16:43:56 -0400

    created test verifying inital message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,7 +5,9 @@ const ganache = require('ganache-cli');
 //use constructor to make instances the web3 library, each instance connects to networks.
 const Web3 = require('web3');
 //connect to this specific test network on local machine
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const {interface , bytecode } = require('../compile');
 
 //Mocha refresher 
@@ -39,6 +41,7 @@ const {interface , bytecode } = require('../compile');
 // });
 let accounts;
 let inbox;
+
 beforeEach(async () =>{
     //Get list of accounts
    accounts = await web3.eth.getAccounts()
@@ -46,11 +49,18 @@ beforeEach(async () =>{
     //Use account to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({data: bytecode, arguments: ['Hi there!']})
-        .send({from: accounts[0], gas: '1000000'})
+        .send({from: accounts[0], gas: '1000000'});
+
+    inbox.setProvider(provider);
 });
 
+//checks for address on contract to validate contract has deployed. 
 describe('Inbox', () =>{
     it('deploys a contract', () =>{
-        console.log(inbox);
+    assert.ok(inbox.options.address);
+    });
+    it('has a default message', async () =>{
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there!');
     });
 });
