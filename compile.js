d9314ca75111229163c8c4b5e9ded9099d42b32a
Tony Thompson <penguinprofession@icloud.com>
2018-06-01 19:45:03 -0400

    created intial test for local test deployment

--- compile.js
+++ compile.js
@@ -1,16 +1,9 @@
 const path = require('path');
-//helps build path from current directory to .sol file, we are guranteed crossed platform compatibility
-
 const solc = require('solc');
 const fs = require('fs');
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
-//this will take you from the home directory on machine to inbox and contracts folder.
+const lottoPath = path.resolve(__dirname, 'contracts', 'Lotto.sol')
+const source = fs.readFileSync(lottoPath, 'utf8');
 
-const source = fs.readFileSync(inboxPath, 'utf8');
-//reads raw source code from our contract by creating source using fs module. 
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
-//exports objects details (contracts property inbox) for direct access.
-//return value will always be an object, top level contracts{ inbox...}
-//provides bytecode and interface (ABI) - list out all functions that can be called, arguements, return values, etc. 
+module.exports = solc.compile(source, 1).contracts[':Lotto'];
---
+++ test/Lotto.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+//ABI and Bytecode Object
+const {interface, bytecode} = require('.../compile');
+
+let lotto;
+let accounts;
+
+beforeEach(async () =>{
+    accounts = await web3.eth.getAccounts();
+
+    lotto = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode})
+    .send({from: accounts[0], gas: '1000000'});
+});
+
+describe('Lotto Contract', () =>{
+    it('deploys a contract', () =>{
+        assert.ok(lotto.options.address);
+    });
+});
