4401b336eb77e70df2ab4acb128a414a0fe74851
Tony Thompson <penguinprofession@icloud.com>
2018-06-01 19:52:40 -0400

    created test to validate account can enter

--- test/Lotto.test.js
+++ test/Lotto.test.js
@@ -21,4 +21,16 @@ describe('Lotto Contract', () =>{
     it('deploys a contract', () =>{
         assert.ok(lotto.options.address);
     });
+    it('allows an account to enter', async () =>{
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(1, players.length);
+    });
 });
