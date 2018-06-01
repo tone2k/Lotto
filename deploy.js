e587f17fd6ff10928d736e9599a1dc4535ca2466
Tony Thompson <penguinprofession@icloud.com>
2018-06-01 10:28:30 -0400

    adjusted pneumonic string

--- deploy.js
+++ deploy.js
@@ -6,7 +6,7 @@ const {interface, bytecode} = require('./compile');
 
 //set provider to new instance with two arguments, account pneumonic and network URL
 const provider = new HDWalletProvider(
-    ‘grape economy stand shoe gentle dove load property swap scene meadow brain’,
+    'grape economy stand shoe gentle dove load property swap scene meadow brain',
     'https://rinkeby.infura.io/UlvBieevamkPWqB0vIyT'
 );
 
