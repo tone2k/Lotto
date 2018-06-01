9442059514555be7f72dadaf465b55512893c85c
Tony Thompson <penguinprofession@icloud.com>
2018-06-01 10:27:25 -0400

    created deploy function with logs mentioning address connecting to

--- deploy.js
+++ deploy.js
@@ -12,3 +12,20 @@ const provider = new HDWalletProvider(
 
 //takes provider, passes it to Web3 constructor and creates new instance of web3 enabled for unlocked rinkbey network
 const web3 = new Web3(provider);
+
+
+//to use async await syntax, created function to deploy. 
+const deploy = async () =>{
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account ', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        //contains objects byte code and any inital arguments to pass to the contract. 
+        .deploy({ data: bytecode, arguments: ['Hello World!']})
+        .send({gas: '1000000', from: accounts[0]});
+
+    console.log('Contract deployed to ', result.options.address);
+};
+
+deploy();
